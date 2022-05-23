7044e46843057a16f479d38be4299989bafdccab
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-23 14:31:43 +0300

    ft:migartions - maps models to datbase tables

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#Editor model this will contain data of our editor
+class Editor(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.first_name
