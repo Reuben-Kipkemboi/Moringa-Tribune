1a657d46bf3a84d613bb31274f1d6511a3bce669
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-23 12:32:10 +0300

    ft:navbar - creates a navbar

--- news/views.py
+++ news/views.py
@@ -5,19 +5,6 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
-
-# #converting our dates
-# def convert_dates(dates):
-
-#     # Function that gets the weekday number for the date.
-#     day_number = dt.date.weekday(dates)
-
-#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-#     # Returning the actual day of the week
-#     day = days[day_number]
-#     return day
-#news of the day function
 def news_of_day(request):
     date = dt.date.today()
     return render(request, 'all-news/today-news.html', {"date": date,})
