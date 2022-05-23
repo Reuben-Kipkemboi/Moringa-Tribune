c9c0d5182b20c7053cc6e02b2b16bfe7da982fbc
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-23 11:34:45 +0300

    ft:template-inheritance - initilizes a base,html file

--- news/views.py
+++ news/views.py
@@ -1,11 +1,11 @@
+from turtle import title
 from django.shortcuts import render
 from django.http  import HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    # return HttpResponse('Welcome to the Moringa Tribune')
-    return render(request, 'welcome.html')
+    return render(request,'all-news/welcome.html')
 
 #converting our dates
 def convert_dates(dates):
